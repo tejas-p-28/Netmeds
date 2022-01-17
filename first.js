5c60ee6cdd5f970448f6fedcd0aa4e8dacfa21d9
Tejas <tejas.patiltp2802@gmail.com>
2022-01-17 11:02:25 +0530

    Netmeds

--- first.js
+++ first.js
@@ -1,9 +1,41 @@
-
+/*Dark Mode Javascript Starts*/
 function changeMode() {
     var mybody = document.body;
     mybody.classList.toggle("mydark")
 }
+/*Dark Mode Javascript Ends*/
+/* Weather Javascript Starts */
 
+var x = document.getElementById("out")
+var y = document.getElementById("address")
+var z = document.getElementById('icon')
+
+function geolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data) {
+    console.log(data)
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude
+    var url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            data.list.map((item) => {
+                console.log(item.temp.day)
+                y.innerText = `${item.temp.day}°C`
+                z.innerHTML = `<img class='card-img-top' src='https://openweathermap.org/img/w/${item.weather[0].icon}.png' alt='weather'/>`
+            })
+
+        })
+}
+/* Weather Javascript Ends */
 
 function mice() {
     document.getElementById('card').style.transform = "scale(1.1)"
@@ -56,52 +88,37 @@ $(document).ready(function () {
 let url = "http://localhost:1255/Products";
 
 
-
-function getProduct(){
+function getProduct() {
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        for(i=0;i<data.length;i++){
-            let element = document.createElement('Option')
-            let text = document.createTextNode(data[i].name)
-            element.appendChild(text)
-            element.value = data[i].id
-            document.getElementById('product').appendChild(element)
+        .then((res) => res.json())
+        .then((data) => {
+            for (i = 0; i < data.length; i++) {
+                let element = document.createElement('Option')
+                let text = document.createTextNode(data[i].name)
+                element.appendChild(text)
+                element.value = data[i].id
+                document.getElementById('product').appendChild(element)
 
-        }
-    })
+            }
+        })
 }
 
-// const getHotel = () => {
-//     const cityId = document.getElementById('city').value;
-//     while(hotels.length>0){
-//         hotels.remove(0)
-//     }
-//     fetch(`${hotelUrl}${cityId}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//         for(i=0;i<data.length;i++){
-//             let element = document.createElement('option')
-//             let text = document.createTextNode(`${data[i].name} | ${data[i].city_name}`)
-//             element.appendChild(text)
-//             document.getElementById('hotels').appendChild(element)
-//         }
-//     })
-// }
+// const productList = "http://localhost:1255/Products";
 
-let productList = () => {
-    let productId = document.getElementById('list').value;
-    while(list.length>0){
+
+const productList = () => {
+    const product_name = document.getElementById('product').value;
+    while (list.length > 0) {
         list.remove(0)
     }
-    fetch(`${url}${productId}`)
-    .then((res) => res.json())
-    .then((data) => {
-        for(i=0;i<data.length;i++){
-            let element = document.createElement('option')
-            let text = document.createTextNode(data[i].product_name)
-            element.appendChild(text)
-            document.getElementById('list').appendChild(element)
-        }
-    })
+    fetch(`${productList}${product_name}`)
+        .then((res) => res.json())
+        .then((data) => {
+            for (i = 0; i < data.length; i++) {
+                let element = document.createElement('option')
+                let text = document.createTextNode(data[i].product)
+                element.appendChild(text)
+                document.getElementById('list').appendChild(element)
+            }
+        })
 }
